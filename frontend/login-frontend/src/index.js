5113f4675cdaefb1e1dfb3c298003b92c018bde2
Filip <szymanski.fp@gmail.com>
2022-03-17 22:24:07 +0100

    wrapped App route in AuthProvider

--- frontend/login-frontend/src/index.js
+++ frontend/login-frontend/src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App";
 import './index.css';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {AuthProvider} from "./context/AuthProvider";
 
 
-ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <AuthProvider>
+            <Routes>
+                <Route path="/*" element={<App/>}/>
+            </Routes>
+        </AuthProvider>
+    </BrowserRouter>
+    , document.getElementById('root'));
 
