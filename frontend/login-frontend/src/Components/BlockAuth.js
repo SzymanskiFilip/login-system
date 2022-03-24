046652fda77dc065fa0dc353b38e1c6d17d5b9b3
Filip <szymanski.fp@gmail.com>
2022-03-24 22:32:33 +0100

    redirecting /login when the session is valid

--- frontend/login-frontend/src/Components/BlockAuth.js
+++ frontend/login-frontend/src/Components/BlockAuth.js
@@ -1,10 +1,26 @@
 import {Navigate} from "react-router-dom";
-import {useContext} from "react";
+import {useContext, useEffect} from "react";
 import {AuthContext} from "../context/Context";
 
 function BlockAuth({children}){
 
     const {authenticated, setAuthenticated} = useContext(AuthContext);
+
+    async function handleCookie(){
+        await fetch("http://localhost:8080/check", {
+            method: "POST",
+            credentials: 'include',
+        }).then(res => {
+            console.log(res.status);
+            if(res.status === 200){
+                setAuthenticated(true);
+            }
+        });
+    }
+
+    useEffect(() => {
+        handleCookie();
+    },[]);
 
     return authenticated === false ? children : <Navigate to="/private"/>
 }
